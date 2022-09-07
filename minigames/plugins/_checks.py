d0ae596f37b85ad2897ce1da62f0c9357129c574
CircuitSacul <circuitsacul@icloud.com>
2022-09-07 16:14:22 +0000

    fix check signatures

--- minigames/plugins/_checks.py
+++ minigames/plugins/_checks.py
@@ -6,20 +6,22 @@ import crescent
 import hikari
 
 
-async def guild_only(ctx: crescent.Context) -> crescent.HookResult | None:
+async def guild_only(ctx: crescent.Context) -> crescent.HookResult:
     if not ctx.guild_id:
         await ctx.respond(
             "This command can only be used inside servers.", ephemeral=True
         )
-        return crescent.HookResult(True)
+        must_exit = True
+    else:
+        must_exit = False
 
-    return None
+    return crescent.HookResult(exit=must_exit)
 
 
 def has_guild_perms(
     perms: hikari.Permissions,
-) -> Callable[[crescent.Context], Awaitable[crescent.HookResult | None]]:
-    async def check(ctx: crescent.Context) -> crescent.HookResult | None:
+) -> Callable[[crescent.Context], Awaitable[crescent.HookResult]]:
+    async def check(ctx: crescent.Context) -> crescent.HookResult:
         await guild_only(ctx)
         assert ctx.guild_id is not None
         assert ctx.member is not None
@@ -33,8 +35,9 @@ def has_guild_perms(
                 "You don't have permission to use this command.",
                 ephemeral=True,
             )
-            return crescent.HookResult(True)
-
-        return None
+            must_exit = True
+        else:
+            must_exit = False
+        return crescent.HookResult(exit=must_exit)
 
     return check
